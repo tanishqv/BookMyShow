c39b8e6626d74f3e0f5b0a634a76cfeca0f480c4
tanishqv <tanishq7verma@gmail.com>
2025-05-24 23:11:07 +0530

    added auditing event listeners

--- src/main/java/com/scaler/BookMyShow/models/BaseModel.java
+++ src/main/java/com/scaler/BookMyShow/models/BaseModel.java
@@ -1,21 +1,25 @@
 package com.scaler.BookMyShow.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+    @LastModifiedDate
     private Date updatedAt;
 }
