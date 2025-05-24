96aea3f0a1148fb0b71d8a7f240f0ec2ccc3d69f
tanishqv <tanishq7verma@gmail.com>
2025-05-24 23:52:55 +0530

    added exception handling

--- src/main/java/com/scaler/BookMyShow/controllers/BookingController.java
+++ src/main/java/com/scaler/BookMyShow/controllers/BookingController.java
@@ -2,6 +2,12 @@ package com.scaler.BookMyShow.controllers;
 
 import com.scaler.BookMyShow.dtos.BookMovieTicketRequestDTO;
 import com.scaler.BookMyShow.dtos.BookMovieTicketResponseDTO;
+import com.scaler.BookMyShow.dtos.SignupResponseDTO;
+import com.scaler.BookMyShow.exceptions.SeatNotAvailableException;
+import com.scaler.BookMyShow.exceptions.ShowNotFoundException;
+import com.scaler.BookMyShow.exceptions.UserAlreadyExistsException;
+import com.scaler.BookMyShow.exceptions.UserNotFoundException;
+import com.scaler.BookMyShow.models.Booking;
 import com.scaler.BookMyShow.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +18,18 @@ public class BookingController {
     private BookingService bookingService;
 
     public BookMovieTicketResponseDTO bookMovieTicket(BookMovieTicketRequestDTO bookMovieTicketRequestDTO) {
-        return null;
+        Booking booking;
+        try {
+            booking = bookingService.bookMovie(
+                    bookMovieTicketRequestDTO.getUserId(),
+                    bookMovieTicketRequestDTO.getShowId(),
+                    bookMovieTicketRequestDTO.getSeatIds()
+            );
+        } catch (UserNotFoundException | ShowNotFoundException | SeatNotAvailableException runtimeException) {
+            booking = null;
+        }
+        BookMovieTicketResponseDTO bookMovieTicketResponseDTO = new BookMovieTicketResponseDTO();
+        bookMovieTicketResponseDTO.setBooking(booking);
+        return bookMovieTicketResponseDTO;
     }
 }
--- src/main/java/com/scaler/BookMyShow/controllers/UserController.java
+++ src/main/java/com/scaler/BookMyShow/controllers/UserController.java
@@ -4,6 +4,9 @@ import com.scaler.BookMyShow.dtos.LoginRequestDTO;
 import com.scaler.BookMyShow.dtos.LoginResponseDTO;
 import com.scaler.BookMyShow.dtos.SignupRequestDTO;
 import com.scaler.BookMyShow.dtos.SignupResponseDTO;
+import com.scaler.BookMyShow.exceptions.PasswordMismatchException;
+import com.scaler.BookMyShow.exceptions.UserAlreadyExistsException;
+import com.scaler.BookMyShow.exceptions.UserNotFoundException;
 import com.scaler.BookMyShow.models.User;
 import com.scaler.BookMyShow.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +19,32 @@ public class UserController {
     private UserService userService;
 
     public SignupResponseDTO signUpUser(SignupRequestDTO signupRequestDTO) {
-        User user = userService.signupUser(
-                signupRequestDTO.getName(),
-                signupRequestDTO.getAge(),
-                signupRequestDTO.getEmail(),
-                signupRequestDTO.getPassword()
-        );
+        User user;
+        try {
+            user = userService.signupUser(
+                    signupRequestDTO.getName(),
+                    signupRequestDTO.getAge(),
+                    signupRequestDTO.getEmail(),
+                    signupRequestDTO.getPassword()
+            );
+        } catch (UserAlreadyExistsException userAlreadyExistsException) {
+            user = null;
+        }
         SignupResponseDTO signupResponseDTO = new SignupResponseDTO();
         signupResponseDTO.setUser(user);
         return signupResponseDTO;
     }
 
     public LoginResponseDTO loginUser(LoginRequestDTO loginRequestDTO) {
-        User user = userService.loginUser(
-                loginRequestDTO.getEmail(),
-                loginRequestDTO.getPassword()
-        );
+        User user;
+        try {
+            user = userService.loginUser(
+                    loginRequestDTO.getEmail(),
+                    loginRequestDTO.getPassword()
+            );
+        } catch (UserNotFoundException | PasswordMismatchException runtimeException) {
+            user = null;
+        }
         LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
         loginResponseDTO.setUser(user);
         return loginResponseDTO;
